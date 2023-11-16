87897194d0be3e10b88dc1c2654739818632e024
albel260819 <albel260819@gmail.com>
2023-11-16 19:10:19 +0300

    задание на системы исчисления

--- HomeWorkInformation/HomeWorkInformation.cpp
+++ HomeWorkInformation/HomeWorkInformation.cpp
@@ -1,75 +1,42 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <Windows.h>
-#include <iostream>
-#include <locale.h>
-#include <cstdlib>
 
-std::vector<int> read(std::ifstream& in)
+int toS(int i, int s)
 {
-    std::string tmpDATA;
-    getline(in, tmpDATA);
-    std::vector<int> data;
-    if (in.is_open())
+    int res = 0;
+    while (i != 0)
     {
-        while (getline(in, tmpDATA))
-        {
-            int num1 = std::stoi(tmpDATA);
-            int num2 = std::stoi(std::string(tmpDATA.begin() + tmpDATA.find(' ') + 1, tmpDATA.end()));
-            data.push_back(num1);
-            data.push_back(num2);
-            //std::cout << num1 << "; ";
-            //std::cout << num2 << '\n';
-        }
+        res = res * 10 + i % s;
+        i /= s;
     }
-    return data;
+    return res;
 }
-struct numbers
+int toDec(int i, int s)
 {
-    int number;
-    int delta;
-    bool parity;
-    int second_num;
-};
+    int res = 0;
+    int S = 1;
+    int dec = 1;
+    while (i != 0)
+    {
+        res = res + (i % 10) * S;// *dec;
+        i /= 10;
+        S = S * s;
+        dec *= 10;
+    }
+    return res;
+}
+
+
 int main()
 {
-    std::ifstream numbers_1IN("numbers_2.txt");
-    auto numbers_1 = read(numbers_1IN);
-    numbers_1IN.close();
-
-    std::vector<numbers> delta_1;
-    for (int i = 0; i < numbers_1.size(); i+=2)
+    int max = 0;
+    for (int i = 0; i < 1000; i++)
     {
-        numbers tmp;
-        tmp.number = numbers_1[i] > numbers_1[i + 1] ? numbers_1[i + 1] : numbers_1[i];
-        tmp.second_num = numbers_1[i] < numbers_1[i + 1] ? numbers_1[i + 1] : numbers_1[i];
-        tmp.delta = tmp.second_num - tmp.number;
-        tmp.parity = (tmp.delta % 2) == 1;
-        //std::cout << tmp.number << "; ";
-        //std::cout << tmp.second_num << "; ";
-        //std::cout << tmp.delta << "; ";
-        //std::cout << tmp.parity << "; " << '\n';
-
-        delta_1.push_back(tmp);
+        int x = toS(i, 6);
+        x = x * 10 + x % 10;
+        x = toS(toDec(x, 6), 2);
+        x = x * 10 + x % 10;
+        x = toDec(x, 2);
+        max = x > max && x < 344 ? x : max;
     }
-    int summ = 0;
-    int parity = 0;
-    int min_delta = 10000;
-    for (auto inter : delta_1)
-    {
-        summ += inter.number;
-        if (inter.number % 2 == 0)
-            parity++;
-        else
-            parity--;
-        if ((min_delta > inter.delta) && (inter.parity))
-            min_delta = inter.delta;
-    }
-    if ((summ % 2 == 0) == (parity > 0))
-    {
-        summ += min_delta;
-    }
-    std::cout << summ;
+    std::cout << max;
 }
